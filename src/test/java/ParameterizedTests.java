9b385586eb7fbb7fb8cd7760978e500b14def61d
minamagdi <mina.m.halim642@gmail.com>
2023-07-04 19:17:22 +0300

    new

--- src/test/java/ParameterizedTests.java
+++ src/test/java/ParameterizedTests.java
@@ -1,8 +1,12 @@
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ParameterizedTests {
     @ParameterizedTest(name = "Run:{index} - value:{arguments}") //to make it more readable
     @ValueSource(ints = {1, 5, 7})
@@ -29,6 +33,53 @@ public class ParameterizedTests {
     @ParameterizedTest
     @CsvSource(value = {"mina,29,true", "nermeen,29,false", "madonna,22,true"})
     public void csvSource_StringIntBoolean(String param1, int param2, boolean param3) {
-        System.out.println("param1 : "+param1 + " , param2 : "+param2 +" , param3 : "+param3);
+        System.out.println("param1 : " + param1 + " , param2 : " + param2 + " , param3 : " + param3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"mina magdy,'ali,kamel'", "moheb samy,'ahmed,galal'"})
+    public void csvSource_StringWithComa(String param1, String param2) {
+        System.out.println("param1 : " + param1 + " ,param2 : " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"mina?magdy", "ali?maher"}, delimiter = '?')
+    public void csvSource_StringWithDiffDelimiter(String param1, String param2) {
+        System.out.println("param1 : " + param1 + ", param2: " + param2);
+    }
+
+    @ParameterizedTest
+    //@CsvFileSource(files = "src/test/resources/params/shoppingList.csv",numLinesToSkip = 1)//to skip first line of file in case of one file
+//    @CsvFileSource(files ={"src/test/resources/params/shoppingList.csv",
+//            "src/test/resources/params/shoppingList2.csv"},numLinesToSkip = 1)
+
+    @CsvFileSource(files = "src/test/resources/params/shoppingList3.csv", numLinesToSkip = 1, delimiterString = "___")
+    public void csvSource_StringDoubleIntStringString(String name, Double price,
+                                                      int quantity, String unit,
+                                                      String provider) {
+        System.out.println("name : " + name + " , price : " + price + " , quantity : " + quantity + " , unit : " +
+                unit + " provider: " + provider);
+
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "sourceString")
+    void methodSource_String(String param1) {
+        System.out.println("param1 : " + param1);
+    }
+
+    List<String> sourceString() {
+        return Arrays.asList("tomato", "carrot", "cabbage");
+    }
+    @ParameterizedTest
+    @MethodSource(value = "sourceStringAsStream")
+    void methodSource_StringAsStream(String param1) {
+        System.out.println("param1 : " + param1);
+    }
+    Stream<String> sourceStringAsStream() {
+        return Stream.of("beetRoot","apple","pear");
+    }
+    List<Arguments> sourceString_StringDouble() {
+        return Arrays.asList(Arguments.arguments("tomato",2.0));
     }
 }
